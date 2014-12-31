cc48288a2a5e9a30bf5ea6d3509d4935ec37efca
Robert Culliton <robertculliton@Roberts-MacBook-Pro.local>
2014-12-30 20:24:40 -0500

    first working setup with selenium

---
+++ src/com/selenium/example/defenders.java
@@ -0,0 +1,14 @@
+package com.selenium.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class defenders {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new FirefoxDriver();
+	}
+
+}
