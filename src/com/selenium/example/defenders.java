b5c3cf20818c0e1cd2bfd89c85d26be439d5af77
Robert Culliton <robertculliton@Roberts-MacBook-Pro.local>
2015-01-03 10:44:52 -0500

    recordings from test output

--- src/com/selenium/example/defenders.java
+++ src/com/selenium/example/defenders.java
@@ -1,14 +1,76 @@
 package com.selenium.example;
 
-import org.openqa.selenium.WebDriver;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class defenders {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		WebDriver driver=new FirefoxDriver();
-	}
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://secure.defenders.org/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
+  @Test
+  public void testScript() throws Exception {
+    driver.get(baseUrl + "/site/Advocacy?cmd=display&page=UserAction&id=2809");
+    new Select(driver.findElement(By.id("title"))).selectByVisibleText("Mr.");
+    driver.findElement(By.id("fname")).clear();
+    driver.findElement(By.id("fname")).sendKeys("Robert");
+    driver.findElement(By.id("lname")).clear();
+    driver.findElement(By.id("lname")).sendKeys("Culliton");
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
 }
